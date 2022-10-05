3bb56a6c041b27563e5b6a4cc59c823b231a02f0
Emanuele Villani <manu.lebon@hotmail.it>
2022-10-05 07:26:19 +0200

    hair, move  characterUI
    aggiunto lo scrips ; DisableAllChildrenOfSelectedGameObject per disabilitare l'oggetto precedente . aggiunto scripts: RotedObject per rotare il characterUI nel panel right

---
+++ Assets/ScriptsEmanuele/CharacterScripts/RotedObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotedObject : MonoBehaviour
+{
+    PlayerControls playerControls;
+
+    Vector2 cameraInput;
+    Vector3 currentRotation;
+    Vector3 targetRotation;
+
+
+    public float rotationAmount = 1;
+    public float rotationSpeed = 5;
+
+
+   
+
+    private void OnEnable()
+    {
+        if(playerControls == null)
+        {
+            playerControls = new PlayerControls();
+            playerControls.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+        }
+        playerControls.Enable();
+    }
+
+    private void Start()
+    {
+        currentRotation = transform.eulerAngles;
+        targetRotation = transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+       if (cameraInput.x > 0)
+        {
+            targetRotation.y = targetRotation.y + rotationAmount;
+
+        }
+        else if (cameraInput.x < 0)
+        {
+            targetRotation.y = targetRotation.y - rotationAmount;
+        }
+
+        currentRotation = Vector3.Lerp(currentRotation, targetRotation, rotationSpeed * Time.deltaTime);
+        transform.eulerAngles = currentRotation;
+    }
+
+
+}
--- Assets/ScriptsEmanuele/PlayerMovement/InputManager.cs
+++ Assets/ScriptsEmanuele/PlayerMovement/InputManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerControls playerControls;
+
+    public Vector2 movementInput;
+
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void OnEnable()
     {
-        
+        if(playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+        playerControls.Enable();
+       
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void HandleAllInputs()
     {
-        
+        HandleMovementInput();
+
+        //HandleJumpingInput
+        //HandleActionInput
+    }
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
     }
 }
+
+
+  
